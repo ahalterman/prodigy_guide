db17f5cb99252011333a0ac97cddc75c5b74b0bb
Andy <ahalterman0@gmail.com>
2018-06-07 22:08:52 -0400

    Add plac code and documentation

--- 2_Wiki/README.md
+++ 2_Wiki/README.md
@@ -1,5 +1,41 @@
+# Wikipedia Example
 
+This example shows how to take in a Wikipedia category page, generate
+Prodigy-formatted JSONL to display them as iframes, and run a simple
+classification display on them.
+
+### Formatting tasks 
+
+Prodigy expects tasks to come in a certain format. Here, where we're displaying
+an external website as an iframe, we need to format it as a JSONL file (newline
+delimited JSON) with an `html` frame containing valid HTML with an iframe.
+
+`wiki_to_task.py` is example code to do this task:
 
 ```
-prodigy mark ucsd_wiki wiki_tasks.jsonl --label "RURAL"
+python wiki_to_task.py -i https://en.wikipedia.org/wiki/Category:Towns_in_Oklahoma -o wiki_tasks.jsonl
 ```
+
+where the link following `-i` is the Wikipedia category page to scrape, and the
+file following `-o` is the file to write it out to.
+
+### Running Prodigy
+
+Once our Wikipedia links are formatted in the correct JSONL format, we can
+start a Prodigy labeling session:
+
+```
+prodigy mark ucsd_wiki wiki_tasks.jsonl --label "SUBURBAN"
+```
+
+where
+
+- `mark`: the Prodigy recipe to use (here, the no model-in-the-loop
+classification model)
+- `ucsd_wiki`: the name of the Prodigy dataset to store the annotations in
+    (will be created automatically if this command is being run for the first
+    time)
+- `wiki_tasks.jsonl`: the file of tasks to annotate that we created above
+- `--label "SUBURBAN"`: the label to use in annotating the examples
+
+
--- 2_Wiki/wiki_to_task.py
+++ 2_Wiki/wiki_to_task.py
@@ -1,26 +1,63 @@
 import requests
 import jsonlines
 from bs4 import BeautifulSoup
+import plac
 
 def cat_to_links(category_url):
+    """
+    Find all the page urls linked to from a Wikipedia category page
+
+    Parameters
+    ----------
+    category_url: str
+        URL of a Wikipedia category page
+
+    Returns
+    -------
+    links: list
+        list of Wikipedia page URLs
+    """
     category = BeautifulSoup(requests.get(category_url).content, "lxml")
     links = category.find("div", {"id": "mw-pages"}).find_all("a", href = True)
     links = ['https://en.wikipedia.org' + i['href'] for i in links][1:]
     return links
 
 def results_to_task(links)
-    output = []
+    """
+    Take in URLs and return Prodigy-formatted tasks with an iframe displaying the page.
+
+    Parameters
+    ---------
+    links: list
+        list with URLs (not necessarily wikipedia) to display in Prodigy as iframes
+
+    Returns
+    -------
+    tasks: list
+        Prodigy formatted JSON with iframes
+    """
+    tasks = []
     for l in links:
         html = "<iframe width='960' height='415' src='{0}'></iframe>".format(l),
         task = {
             "html": html,
             "text": l
         }
-        output.append(task)
+        tasks.append(task)
     return links
 
-if __name__ == "__main__":
-    links = cat_to_links("https://en.wikipedia.org/wiki/Category:Towns_in_Oklahoma")
+@plac.annotations(
+    cat_url("Wikipedia category URL", "option", "i", str),
+    output_file("File to write Prodigy JSONL to", "option", "o", str))
+def main(cat_url, output_file):
+    """
+    Go from Wikipedia category page to Prodigy JSONL
+    """
+    links = cat_to_links(cat_url)
     output = results_to_tasks(links)
-    with jsonlines.open("wiki_tasks.jsonl", mode='w') as writer:
+    with jsonlines.open(output_file, mode='w') as writer:
         writer.write_all(output)
+
+
+if __name__ == '__main__':
+    plac.call(main)
